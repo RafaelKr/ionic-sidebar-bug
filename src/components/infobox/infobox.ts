34ab9afc1f7989e68a5771026be1d035cbb30701
Rafael Kraut <rk@suedsolutions.de>
2016-12-06 17:51:08 +0100

    first commit

---
+++ src/components/infobox/infobox.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/*
+  Generated class for the Infobox component.
+
+  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+  for more info on Angular 2 Components.
+*/
+@Component({
+  selector: 'infobox',
+  templateUrl: 'infobox.html'
+})
+export class InfoboxComponent {}
